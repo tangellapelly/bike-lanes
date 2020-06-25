78d85b62e8412dcda601379bda8d89762c3c7024
dbabbs <dylan.babbs@gmail.com>
2020-06-25 09:44:33 -0700

    add build & docs

---
+++ build/precache-manifest.5012c9b252259baf616b51418389c863.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ced6a37ab9c8bd96ae8f9ddec730c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec69b2a9438eaaf7e229",
+    "url": "/static/css/main.5e93c764.chunk.css"
+  },
+  {
+    "revision": "eab75e631fe41bc57b7e",
+    "url": "/static/js/2.85334d04.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.85334d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec69b2a9438eaaf7e229",
+    "url": "/static/js/main.6220e3e5.chunk.js"
+  },
+  {
+    "revision": "ddb617218c9a1b1e5feb",
+    "url": "/static/js/runtime-main.53a3600a.js"
+  },
+  {
+    "revision": "150b8dd92d40ea0969d5e95a22ed7897",
+    "url": "/static/media/style.150b8dd9.yaml"
+  }
+]);
